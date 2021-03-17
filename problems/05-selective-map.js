b3b909b843d91d8947e52157740481f33da7f340
Scilaer <Scilaerg@gmail.com>
2021-03-17 15:28:45 -0500

    05 Completed

--- problems/05-selective-map.js
+++ problems/05-selective-map.js
@@ -8,7 +8,26 @@ in false, then the element should not be changed in the new array.
 Note that that you cannot use the Array `map` or `filter` methods to solve this
 problem.
 
-Examples:
+AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
+    (optional if you already asked a question for this problem.)
+- Explain how you are using both of the callbacks in the function.
+- What do you expect each callback function to be returning?
+- How many times are you calling each callback function?
+*******************************************************************************/
+
+let selectiveMap = function(arr, cb1, cb2) {
+    let newArr = []
+    for ( let i = 0; i < arr.length; i++ ) {
+        let el = arr[i]
+        if (cb1(el)) {
+            newArr.push(cb2(el))
+        } else {
+            newArr.push(el)
+        }
+    }
+    return newArr
+};
+
 
 function isEven(n) {
     return n % 2 === 0;
@@ -34,20 +53,6 @@ console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isEven, flipSign));
 
 console.log(selectiveMap([-10, 4, 7, 6, -2, -9], isPositive, square));
 // [-10, 16, 49, 36, -2, -9]
-
-AFTER YOU ARE FINISHED WITH THIS PROBLEM, ASK FOR A CODE REVIEW
-    (optional if you already asked a question for this problem.)
-- Explain how you are using both of the callbacks in the function.
-- What do you expect each callback function to be returning?
-- How many times are you calling each callback function?
-*******************************************************************************/
-
-let selectiveMap = function() {
-
-};
-
-
-
 
 
 
